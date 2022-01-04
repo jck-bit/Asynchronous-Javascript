008fbef49f11c13b7ed8231028a550b483d0a6ee
jck-bit <jckkinyanjui@gmail.com>
2022-01-04 23:17:40 +0300

    Promises

--- Promises.js
+++ Promises.js
@@ -1,31 +1,27 @@
 const getTodos = (resorce) =>{
 
-    return new Promise((resolve, reject) =>{
-        const request = new XMLHttpRequest();
-        request.addEventListener('readystatechange', ()=>{
-        
-          if( request.readyState ===4 &&request.status ===200){
-            const data = JSON.parse(request.responseText)
-            resolve(data);
-        
-          } else if(request.readyState === 4){
-             reject('error getting results')
-          }
-        })
-        
-        request.open('GET',resorce)
-        request.send();
-        
-    });
-};
 
-getTodos('todos/todos.json')
-.then(data=>{
-   console.log('promise 1 resolved',data)
+   return new Promise ((resolve, reject)=>{
+      const request = new XMLHttpRequest();
+ 
+       request.addEventListener('readystatechange', () =>{
+ 
+       if(request.readyState ===4 && request.status ===200 ){
+         const data = JSON.parse(request.responseText)
+         resolve(data)
+       }else if(request.readyState === 4){
+         reject('error getting resources')
+       }
+   })
+   
+   request.open('GET', resorce);
+   request.send();  
+   })
+ };
 
- return getTodos('todos/marios.json');
-}).then(data =>{
-   console.log('promise 2 resolved', data);
-}).catch(err=>{
-   console.log('promise rejected ,', err)
-})
+ getTodos('todos/todos.json').then(data =>{
+   console.log('promise resolved', data)
+ }).catch((err) =>{
+    console.log('promise rejected:', err)
+ });
+
